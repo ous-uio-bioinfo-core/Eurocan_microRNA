4509e10a870f032a4821951cc549038a70530eef
vegardny <vegardsemail@gmail.com>
2015-09-14 13:43:20 +0200

    revision1

--- adhocscripts/sampleannotationmapping.R
+++ adhocscripts/sampleannotationmapping.R
@@ -26,3 +26,37 @@ sa=read.table(paste(annotdir,  "/", sampleannotation_file, sep=""),
 sa$iClust = NA
 sa$iClust = bigsa$iClust[ match(sa$fulldatapath, bigsa$fulldatapath) ]
 write.table(sa, file=sampleannotation_file, col.names=TRUE, row.names=FALSE, quote=FALSE, sep="\t")
+
+
+# attach pam50 scores that where calculated from mRNA samples in a related project.
+UCAM_mRNAannto=read.table("../not_in_github/mRNAannot/clinical-data-illumina-with-subtyping.txt",
+								 sep="\t", header=TRUE, stringsAsFactors=FALSE, fill=TRUE,
+								 strip.white=TRUE, comment.char ="")
+All_mRNAannto=read.table("../not_in_github/mRNAannot/sampleannotation_w_subtype_agilent.txt",
+													sep="\t", header=TRUE, stringsAsFactors=FALSE, fill=TRUE,
+													strip.white=TRUE, comment.char ="")
+
+# check that the UCAMs pam50 still are the same
+UCAM_mRNAannto$similarid = gsub("MB-", "UCAM_MB.", UCAM_mRNAannto$sample_id)
+a = match( UCAM_mRNAannto$similarid, sa$sample_id)
+UCAM_mRNAannto = UCAM_mRNAannto[!is.na(a),]
+a = match( UCAM_mRNAannto$similarid, sa$sample_id)
+UCAM_mRNAannto$subtype.est = gsub("Basal", "Basallike", UCAM_mRNAannto$subtype.est)
+UCAM_mRNAannto$subtype.est = gsub("Normal", "Normallike", UCAM_mRNAannto$subtype.est)
+table(UCAM_mRNAannto$subtype.est==sa$pam50_est[a]) # ok.
+sa$accountedfor=FALSE
+sa$accountedfor[a] = TRUE
+
+a =match(All_mRNAannto$sample_id, sa$sample_id)
+All_mRNAannto = All_mRNAannto[!is.na(a),]
+a =match(All_mRNAannto$sample_id, sa$sample_id)
+All_mRNAannto$sub = gsub("^Basal$", "Basallike", All_mRNAannto$sub)
+All_mRNAannto$sub = gsub("^Normal$", "Normallike", All_mRNAannto$sub)
+b =(!is.na(All_mRNAannto$sub) & All_mRNAannto$sub!=sa$pam50_est[a])
+data.frame(All_mRNAannto$sample_id, All_mRNAannto$sub, sa$pam50_est[a], sa$tissue_type[a])[b,]
+b =(!is.na(All_mRNAannto$sub) & All_mRNAannto$sub==sa$pam50_est[a])
+sa$accountedfor[a][b] = TRUE
+
+
+sa[!sa$accountedfor, c("sample_id", "pam50_est", "tissue_type", "accountedfor")]
+
